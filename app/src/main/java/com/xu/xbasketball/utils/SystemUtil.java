40dfdd2ea5c22d88545a2bcf7c81201aecda78e8
XuDeveloper <280745397@qq.com>
2019-06-16 22:06:32 +0800

    [UPDATE]编写滑动退出支持库（1）

--- app/src/main/java/com/xu/xbasketball/utils/SystemUtil.java
+++ app/src/main/java/com/xu/xbasketball/utils/SystemUtil.java
@@ -2,6 +2,8 @@ package com.xu.xbasketball.utils;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by xu on 2019/03/17.
@@ -14,6 +16,13 @@ public class SystemUtil {
         PackageManager pm = context.getPackageManager();
         return (PackageManager.PERMISSION_GRANTED ==
                 pm.checkPermission(permission, "com.xu.xbasketball"));
+    }
+
+    public static void hideInputMethod(Context context, View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null && view != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
 }
--- app/src/main/java/com/xu/xbasketball/widget/ActivitySwipeBackHelper.java
+++ app/src/main/java/com/xu/xbasketball/widget/ActivitySwipeBackHelper.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 
+import com.xu.xbasketball.utils.SystemUtil;
+
 /**
  * Created by xu on 2019/03/24.
  * <p>
@@ -13,10 +15,14 @@ import android.view.ViewConfiguration;
  */
 public class ActivitySwipeBackHelper {
 
+    private static final int SWIPE_STATE_ACTION_DOWN = 1;
+
     private AppCompatActivity mActivity;
     private float mLastPointX;
     private int mTouchSlop;
     private SwipeBackCallback callback;
+
+    private boolean isSwipeBack = false;
 
     public ActivitySwipeBackHelper(AppCompatActivity activity, SwipeBackCallback callback) {
         this.mActivity = activity;
@@ -30,6 +36,9 @@ public class ActivitySwipeBackHelper {
             mLastPointX = ev.getRawX();
         }
         switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                processSwipeBackEvent(SWIPE_STATE_ACTION_DOWN);
+                break;
             case MotionEvent.ACTION_MOVE:
                 final float curPointX = ev.getRawX();
                 if (Math.abs(curPointX - mLastPointX) < mTouchSlop) {
@@ -37,12 +46,24 @@ public class ActivitySwipeBackHelper {
                 } else {
 //                    Log.i("Activity", "curPointX: " + curPointX);
 //                    Log.i("Activity", "mLastPointX: " + mLastPointX);
+                    isSwipeBack = true;
                     callback.onSwipeBack();
                     return true;
                 }
-
+            default:
+                isSwipeBack = false;
+                break;
         }
         return false;
+    }
+
+    private void processSwipeBackEvent(int eventType) {
+        switch (eventType) {
+            case SWIPE_STATE_ACTION_DOWN:
+                // 预处理
+                SystemUtil.hideInputMethod(mActivity, mActivity.getCurrentFocus());
+                break;
+        }
     }
 
     public interface SwipeBackCallback {
